7c4575fb01c5b1a1a5fded8e53b3467d9ec28c4f
Rareș Cosma <rares@getbetter.ro>
2020-11-07 18:28:35 +0100

    Version through pyproject.toml

--- src/der_py/__init__.py
+++ src/der_py/__init__.py
@@ -1,2 +1,7 @@
 """Just a derpy python project."""
-__version__: str = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version  # type: ignore
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
