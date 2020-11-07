fe87311f4f31b76b9fab7ee9d52313c6ba08fdec
Rareș Cosma <rares@getbetter.ro>
2020-11-07 15:11:42 +0100

    Add an extra type annotation checker through flake8

--- noxfile.py
+++ noxfile.py
@@ -34,6 +34,7 @@ def lint(session: Session) -> None:
     _install_with_constraints(
         session,
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
