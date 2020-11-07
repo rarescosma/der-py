4d73c686c757d6e1b3dab2ff4987b08b1f053f11
Rareș Cosma <rares@getbetter.ro>
2020-11-07 14:58:25 +0100

    Deserialize using marshmallow + desert

--- src/der_py/clients/wiki.py
+++ src/der_py/clients/wiki.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
+from functools import lru_cache
+from typing import Type
 
 import click
 import requests
+from desert import schema
+from marshmallow import EXCLUDE, Schema, ValidationError
 
 API_URL: str = (
     "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
@@ -18,10 +22,11 @@ def random_page(language: str = "en") -> Page:
     try:
         with requests.get(API_URL.format(language=language)) as response:
             response.raise_for_status()
-            data = response.json()
-            return Page(
-                title=data.get("title", ""),
-                extract=data.get("extract", ""),
-            )
-    except requests.RequestException as err:
+            return _schema(of=Page).load(response.json())
+    except (requests.RequestException, ValidationError) as err:
         raise click.ClickException(str(err)) from err
+
+
+@lru_cache(maxsize=64)
+def _schema(of: Type) -> Schema:
+    return schema(of, meta={"unknown": EXCLUDE})
--- tests/clients/test_wiki.py
+++ tests/clients/test_wiki.py
@@ -1,3 +1,6 @@
+import click
+import pytest
+
 from der_py.clients import wiki
 
 
@@ -10,3 +13,11 @@ def test_random_page_uses_given_language(mock_requests_get):
 def test_random_page_returns_page(mock_requests_get):
     page = wiki.random_page()
     assert isinstance(page, wiki.Page)
+
+
+def test_random_page_handles_validation_errors(mock_requests_get):
+    mock_requests_get.return_value.__enter__.return_value.json.return_value = (
+        None
+    )
+    with pytest.raises(click.ClickException):
+        wiki.random_page()
