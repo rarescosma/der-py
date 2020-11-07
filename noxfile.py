8da114ea6cd2a45123abf3c27b87f2493b48d5f8
Rareș Cosma <rares@getbetter.ro>
2020-11-07 10:14:39 +0100

    Test + lint + blacken via flake8 + nox

---
+++ noxfile.py
@@ -0,0 +1,27 @@
+import nox
+
+
+nox.options.sessions = "lint", "tests"
+LOCATIONS = "src", "tests", "noxfile.py"
+PYTHONS = ["3.8"]
+
+
+@nox.session(python=PYTHONS)
+def tests(session):
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    # session.run("poetry", "install", external=True)
+    session.run("pytest", "tests", *args)
+
+
+@nox.session(python=PYTHONS)
+def lint(session):
+    args = session.posargs or LOCATIONS
+    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.run("flake8", *args)
+
+
+@nox.session(python=PYTHONS)
+def black(session):
+    args = session.posargs or ("--quiet", *LOCATIONS)
+    session.install("black")
+    session.run("black", *args)
