0524735ca7ebaa763935493074450eb3718989bb
Rareș Cosma <rares@getbetter.ro>
2020-11-07 11:44:37 +0100

    Lint & check for security issues using bugbear + bandit + safety

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,9 @@
+import tempfile
+
 import nox
 
 
-nox.options.sessions = "lint", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 LOCATIONS = "src", "tests", "noxfile.py"
 PYTHONS = ["3.8"]
 
@@ -17,12 +19,39 @@ def tests(session):
 @nox.session(python=PYTHONS)
 def lint(session):
     args = session.posargs or LOCATIONS
-    session.install("flake8", "flake8-black", "flake8-import-order")
+    session.install(
+        "flake8",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-import-order",
+    )
     session.run("flake8", *args)
 
 
 @nox.session(python=PYTHONS)
 def black(session):
-    args = session.posargs or ("--quiet", *LOCATIONS)
+    args = session.posargs or LOCATIONS
     session.install("black")
     session.run("black", *args)
+
+
+@nox.session(python=PYTHONS)
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run(
+            "safety",
+            "check",
+            f"--file={requirements.name}",
+            "--full-report",
+        )
