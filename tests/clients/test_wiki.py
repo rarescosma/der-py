963e7046e630ba76b4fec2458f88413e2e9da60d
Rareș Cosma <rares@getbetter.ro>
2021-07-26 13:03:34 +0200

    [test] use requests-mock instead of pytest-mock

--- tests/clients/test_wiki.py
+++ tests/clients/test_wiki.py
@@ -4,20 +4,16 @@ import pytest
 from der_py.clients import wiki
 
 
-def test_random_page_uses_given_language(mock_requests_get):
+def test_random_page_uses_given_language(mock_wiki_get):
     wiki.random_page(language="de")
-    args, _ = mock_requests_get.call_args
-    assert "de.wikipedia.org" in args[0]
+    assert mock_wiki_get.last_request.hostname == "de.wikipedia.org"
 
 
-def test_random_page_returns_page(mock_requests_get):
+def test_random_page_returns_page(mock_wiki_get):
     page = wiki.random_page()
     assert isinstance(page, wiki.Page)
 
 
-def test_random_page_handles_validation_errors(mock_requests_get):
-    mock_requests_get.return_value.__enter__.return_value.json.return_value = (
-        None
-    )
+def test_random_page_handles_validation_errors(mock_wiki_get_bad_json):
     with pytest.raises(click.ClickException):
         wiki.random_page()
